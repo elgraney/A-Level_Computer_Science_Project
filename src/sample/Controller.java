daf00a5f8905478dd4488783c28064869924c644
elgraney <MA.Crane@monmouthschool.org>
2017-01-19 14:30:44 +0000

    display image pool

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,12 +6,19 @@ import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
 
-import javax.swing.*;
-import java.awt.*;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 import java.io.File;
@@ -24,8 +31,18 @@ public class Controller extends BorderPane {
     @FXML private Button importButton;
     @FXML public Label boom;
     @FXML private Button importTemplate;
+    @FXML private ImageView imageFrame0;
+    @FXML private ImageView imageFrame1;
+    @FXML private ImageView imageFrame2;
+    @FXML private ImageView imageFrame3;
+    @FXML private ImageView imageFrame4;
+    @FXML private ImageView imageFrame5;
 
-    List<SImage> imagePool = new ArrayList<SImage>();
+    private List<SImage> SImagePool = new ArrayList<SImage>();
+    private List<Image> imagePool = new ArrayList<Image>();
+
+    private Integer page = 0;
+
 
 
     public Controller() {
@@ -39,12 +56,24 @@ public class Controller extends BorderPane {
         System.out.println(selectedFiles);
         for (File selectedFile : selectedFiles) {
             SImage currentImage = new SImage(selectedFile);
-            imagePool.add(currentImage);
+            try {
+                SImagePool.add(currentImage);
+                imagePool.add(new Image(new FileInputStream(currentImage.file)));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
-        for (SImage image : imagePool){
-            System.out.println("image");
+        updateImagePool(SImagePool, imagePool);
+        }
 
-        }
+
+    public void updateImagePool(List SImagePool, List imagePool){
+
+        imageFrame0.setImage((Image) imagePool.get(0+page));
+        imageFrame1.setImage((Image) imagePool.get(1+page));
+        imageFrame2.setImage((Image) imagePool.get(2+page));
+        imageFrame3.setImage((Image) imagePool.get(3+page));
+
     }
     public void importTemplate(){
         FileChooser fileChooser = new FileChooser();
